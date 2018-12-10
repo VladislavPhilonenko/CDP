0a277b571a414d20d4be42f1570f48ed8dc15baa
Vladyslav Filonenko <Vladyslav_Filonenko@epam.com>
2018-12-10 15:55:38 +0200

    initial setting

---
+++ fake-db/index.js
@@ -0,0 +1,53 @@
+let db = [];
+
+function makeId() {
+    let id = "";
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < 36; i++)
+        id += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return id;
+}
+
+module.exports = {
+    getCollection(cb) {
+        cb(null, db);
+    },
+    getById(id, cb) {
+        let matchedEntries = db.filter(entry => entry.id === id);
+
+        if (matchedEntries.length) {
+            cb(null, matchedEntries[0]);
+        } else {
+            cb(new Error('There is no such record in DB'), null);
+        }
+    },
+    create(model, cb) {
+        model.id = makeId();
+        db.push(model);
+
+        cb(null, model);
+    },
+    update(model, cb) {
+        let matchedModel = db.filter(entry => entry.id === model.id)[0];
+
+        if (matchedModel) {
+            db[db.indexOf(matchedModel)] = model;
+            cb(null, model);
+        } else {
+            cb(new Error('There is no such model'), null)
+        }
+    },
+    remove(id, cb) {
+        let matchedModel = db.filter(entry => entry.id === id)[0];
+
+        if (matchedModel) {
+            db.splice(db.indexOf(matchedModel), 1);
+            cb(null);
+        } else {
+            cb(new Error('There is no such model'))
+        }
+    }
+};
+
