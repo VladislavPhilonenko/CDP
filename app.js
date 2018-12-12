dbbc51f4fe71d83ad2df4048ab1e33a2d13a30e9
Vladyslav Filonenko <Vladyslav_Filonenko@epam.com>
2018-12-12 14:50:25 +0200

    Finish put and delete requests

--- app.js
+++ app.js
@@ -2,69 +2,148 @@ const { Server } = require('http');
 const db = require('./fake-db');
 const port = 3000;
 
-const handleGETRequest = (url, res) => {
-  if (url === '/api/users') {
+const handleError = (code, errText, res) => {
+  console.log(errText);
+
+  res.writeHead(code, errText);
+  res.end();
+};
+
+const handleGETRequest = (req, res) => {
+  if (req.url === '/api/users') {
     db.getCollection((err, collection) => {
+      if (err) {
+        handleError(500, err, res);
+      }
+
       res.end(JSON.stringify(collection));
     });
-  }
-
-  if (url.search(/\/api\/users\/[a-zA-Z0-9]{36}$/) !== -1) {
-    const id = url.match(/[a-zA-Z0-9]{36}$/)[0];
+  } else if (req.url.search(/\/api\/users\/[a-zA-Z0-9]{36}$/) !== -1) {
+    const id = req.url.match(/[a-zA-Z0-9]{36}$/)[0];
 
     db.getById(id, (err, user) => {
       if (err) {
-        console.log(err);
-
-        res.writeHead(500, err);
-        res.end();
+        handleError(500, err, res);
       }
 
       res.end(JSON.stringify(user));
     });
+  } else {
+    handleError(404, 'Not Found', res);
   }
-
-  res.writeHead(404, 'Not Found');
-  res.end();
 };
 
 const handlePOSTRequest = (req, res) => {
   if (req.url === '/api/users') {
-    let body = '';
+    let data = '';
 
-    req.on('data', chunk => {
-      body += chunk.toString();
-    });
+    req.on('error', err => {
+      handleError(400, err, res);
+    }).on('data', chunk => {
+      data += chunk;
+    }).on('end', () => {
+      try {
+        const parsedData = JSON.parse(data);
+        const {
+          firstName,
+          lastName,
+          company,
+          position,
+          email,
+          phoneNumber
+        } = parsedData;
+        const validData = {
+          firstName,
+          lastName,
+          company,
+          position,
+          email,
+          phoneNumber
+        };
 
-    req.on('end', () => {
-      const bodyObj = JSON.parse(body);
-      if (bodyObj instanceof Object) {
-        db.create(bodyObj, (err, user) => {
+        db.create(validData, (err, user) => {
+          if (err) {
+            handleError(500, err.message, res);
+          }
+
           res.end(JSON.stringify(user));
         });
-      } else {
-        res.writeHead(400, 'Invalid data');
-        res.end();
+      } catch (err) {
+        handleError(400, err.message, res);
       }
     });
   } else {
-    res.writeHead(404, 'Not Found');
-    res.end();
+    handleError(404, 'Not Found', res);
   }
 };
 
-const handlePUTRequest = () => {
+const handlePUTRequest = (req, res) => {
+  if (req.url.search(/\/api\/users\/[a-zA-Z0-9]{36}$/) !== -1) {
+    let data = '';
 
+    req.on('error', err => {
+      handleError(400, err, res);
+    }).on('data', chunk => {
+      data += chunk;
+    }).on('end', () => {
+      try {
+        const parsedData = JSON.parse(data);
+        const {
+          id,
+          firstName,
+          lastName,
+          company,
+          position,
+          email,
+          phoneNumber
+        } = parsedData;
+        const validData = {
+          id,
+          firstName,
+          lastName,
+          company,
+          position,
+          email,
+          phoneNumber
+        };
+
+        db.update(validData, (err, user) => {
+          if (err) {
+            handleError(500, err.message, res);
+          }
+
+          res.end(JSON.stringify(user));
+        });
+      } catch (err) {
+        console.log('here');
+        handleError(400, err.message, res);
+      }
+    });
+  } else {
+    handleError(404, 'Not Found', res);
+  }
 };
 
-const handleDELETERequest = () => {
+const handleDELETERequest = (req, res) => {
+  if (req.url.search(/\/api\/users\/[a-zA-Z0-9]{36}$/) !== -1) {
+    const id = req.url.match(/[a-zA-Z0-9]{36}$/)[0];
 
+    db.remove(id, err => {
+      if (err) {
+        handleError(500, err, res);
+      }
+
+      res.end('OK');
+    });
+  } else {
+    handleError(404, 'Not Found', res);
+  }
 };
 
 const server = new Server((req, res) => {
   switch (req.method) {
     case 'GET':
-      handleGETRequest(req.url, res);
+      handleGETRequest(req, res);
 
       break;
     case 'POST':
@@ -72,11 +151,11 @@ const server = new Server((req, res) => {
 
       break;
     case 'PUT':
-      handlePUTRequest(res);
+      handlePUTRequest(req, res);
 
       break;
     case 'DELETE':
-      handleDELETERequest(res);
+      handleDELETERequest(req, res);
 
       break;
     default:
--- fake-db/index.js
+++ fake-db/index.js
@@ -1,62 +1,61 @@
 let db = [{
-    id: 'gq57UMCYYgxMOnLkhKcYJK0SY0EyMUnU8h1b',
-    firstName: 'James',
-    lastName: 'Aren',
-    company: 'Epam',
-    position: 'Software Engineer',
-    email: 'lala@g,ail.com',
-    phoneNumber: '0604 449343 343'
-
+  id: 'gq57UMCYYgxMOnLkhKcYJK0SY0EyMUnU8h1b',
+  firstName: 'James',
+  lastName: 'Aren',
+  company: 'Epam',
+  position: 'Software Engineer',
+  email: 'lala@g,ail.com',
+  phoneNumber: '0604 449343 343'
 }];
 
 function makeId() {
-    let id = '';
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let id = '';
+  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-    for (let i = 0; i < 36; i++)
-        id += possible.charAt(Math.floor(Math.random() * possible.length));
+  for (let i = 0; i < 36; i++)
+    id += possible.charAt(Math.floor(Math.random() * possible.length));
 
-    return id;
+  return id;
 }
 
 module.exports = {
-    getCollection(cb) {
-        cb(null, db);
-    },
-    getById(id, cb) {
-        let matchedEntries = db.filter(entry => entry.id === id);
+  getCollection(cb) {
+    cb(null, db);
+  },
+  getById(id, cb) {
+    let matchedEntries = db.filter(entry => entry.id === id);
 
-        if (matchedEntries.length) {
-            cb(null, matchedEntries[0]);
-        } else {
-            cb(new Error('There is no such record in DB'), null);
-        }
-    },
-    create(model, cb) {
-        model.id = makeId();
-        db.push(model);
-
-        cb(null, model);
-    },
-    update(model, cb) {
-        let matchedModel = db.filter(entry => entry.id === model.id)[0];
-
-        if (matchedModel) {
-            db[db.indexOf(matchedModel)] = model;
-            cb(null, model);
-        } else {
-            cb(new Error('There is no such model'), null)
-        }
-    },
-    remove(id, cb) {
-        let matchedModel = db.filter(entry => entry.id === id)[0];
-
-        if (matchedModel) {
-            db.splice(db.indexOf(matchedModel), 1);
-            cb(null);
-        } else {
-            cb(new Error('There is no such model'))
-        }
+    if (matchedEntries.length) {
+      cb(null, matchedEntries[0]);
+    } else {
+      cb(new Error('There is no such record in DB'), null);
     }
+  },
+  create(model, cb) {
+    model.id = makeId();
+    db.push(model);
+
+    cb(null, model);
+  },
+  update(model, cb) {
+    let matchedModel = db.filter(entry => entry.id === model.id)[0];
+
+    if (matchedModel) {
+      db[db.indexOf(matchedModel)] = model;
+      cb(null, model);
+    } else {
+      cb(new Error('There is no such model'), null)
+    }
+  },
+  remove(id, cb) {
+    let matchedModel = db.filter(entry => entry.id === id)[0];
+
+    if (matchedModel) {
+      db.splice(db.indexOf(matchedModel), 1);
+      cb(null);
+    } else {
+      cb(new Error('There is no such model'))
+    }
+  }
 };
 
